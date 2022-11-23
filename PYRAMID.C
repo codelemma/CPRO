c083e3addacc084d0e421b1115e17f20e1731bcf
Abhishek $ <106401929+codelemma@users.noreply.github.com>
2022-11-24 04:01:13 +0530

    Update PYRAMID.C

--- PYRAMID.C
+++ PYRAMID.C
@@ -1,25 +1,25 @@
+//formation of the pyramid pattern 
 #include<stdio.h>
 #include<conio.h>
-void main()                           //formation of the pattern
+int main()                           
 {
  int i,j,n;
- clrscr();
  printf("Enter the number");
  scanf("%d",&n);
  for(i=1;i<=n;i++)
  {
-  for(j=1;j<=(2*n-1);j++)
-  {
-   if(j>=n-(i-1) && j<=n+(i-1))
+   for(j=1;j<=(2*n-1);j++)
+   {
+    if(j>=n-(i-1) && j<=n+(i-1))
     {
      printf("*");
     }
-     else
+    else
     {
      printf(" ");
     }
-   }
-  printf("\n");
   }
-getch();
+  printf("\n");
+ }
+return 0;
 }
